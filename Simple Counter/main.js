55478caab527b000b97352ffe314c8b3da3238bd
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-23 20:47:17 +0100

    Update main.js

--- Simple Counter/main.js
+++ Simple Counter/main.js
@@ -1,4 +1,5 @@
 const btnE1 = document.querySelector("btn");
+const num_spaceE1 = document.querySelector("num_space");
 
 
 let count = 0
@@ -6,6 +7,6 @@ let count = 0
 
 function UpdateNum(){
     count +=1;
-
+    num_spaceE1.textContent = count;
 
 }
