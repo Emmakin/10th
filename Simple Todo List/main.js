278e0e01c17f486ce412d4423945e999039ad832
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-25 23:29:01 +0100

    Update main.js

--- Simple Todo List/main.js
+++ Simple Todo List/main.js
@@ -4,7 +4,10 @@ let todoInput = document.getElementById("inputValue");
 
 
 todoBtnE1.addEventListener('click', function addTodo(){
-    var paragraph = document.createElement('p')
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
     paragraph.innerText = todoInput.value;
     todoArea.appendChild(paragraph);
+    todoInput.value = "";
+    paragraph.addEventListener('click', function())
 });
