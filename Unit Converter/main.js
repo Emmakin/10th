9d8f3fa70a5d9fb8df0d2c571e4b98249530c4a3
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-27 23:14:55 +0100

    Updates
    Add unit converter

---
+++ Unit Converter/main.js
@@ -0,0 +1,19 @@
+let feet = document.getElementById('feet');
+let inches = document.getElementById('inches');
+
+feet.addEventListener('input', function(){
+    let i = feet.value;
+    let j = i * 12;
+    inches.value = j;
+});
+
+
+inches.addEventListener('input', function(){
+    let i = inches.value;
+    let j = i * 12;
+    feet.value = j;
+});
+
+if(!Number.isInteger(inches.value)){
+    j.tofixed(2);
+}
