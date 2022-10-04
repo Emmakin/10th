efca094a54f2cf7053f9004203b569fffc4b006a
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-04 23:24:10 +0100

    Update main.js

--- Popup Message/main.js
+++ Popup Message/main.js
@@ -1,2 +1,4 @@
 const dataModalButtons = document.querySelectorAll('data-modal-target');
 const dataCloseButtons = document.querySelectorAll('data-close-button');
+const overlay = document.getElementById('overlay');
+
